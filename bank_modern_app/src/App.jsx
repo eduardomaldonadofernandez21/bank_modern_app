8ebf05de974f7ff9bd28297a6ccc9800be29f82b
eduardomaldonadofernandez21 <edumaldonadofdez21@gmail.com>
2024-03-28 10:50:47 +0000

    creation folder components and initial components

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import styles from './style';
 
 function App() {
   return (
     <div className="bg-primary w-full overflow-hidden">
-      <h1>Hello, World!</h1>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
     </div>
   );
 }
